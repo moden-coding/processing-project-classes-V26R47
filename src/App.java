9508a53fa67c5e5d764cbb4699ee2dc5db6d13d4
V26R47 <28vrichmondshazeer@gannacademy.org>
2024-12-05 13:34:56 -0500

    Commit

--- src/App.java
+++ src/App.java
@@ -6,11 +6,23 @@ public class App extends PApplet{
     }
 
     public void setup(){
+        background(255, 0, 255);
+        rect(25, 225,100,100);
+        rect(150,225,100,100);
+        rect(275,225,100,100);
+        rect(25, 350,100,100);
+        rect(150, 350,100,100);
+        rect(275, 350,100,100);
+        rect(25, 475,100,100);
+        rect(150, 475,100,100);
+        rect(275, 475,100,100);
+        textSize(25);
+        text("greater undecimal neutral second", 25,50);
         
     }
 
     public void settings(){
-        
+            size(400,600);
     }
 
     public void draw(){
