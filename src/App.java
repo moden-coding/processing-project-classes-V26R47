1d02087f7d1aecc49b107e8e1082aa4e74f5ba72
V26R47 <28vrichmondshazeer@gannacademy.org>
2024-12-12 22:23:59 -0500

    commit

--- src/App.java
+++ src/App.java
@@ -11,17 +11,26 @@ public class App extends PApplet{
     public void setup(){
         box1 = new Box(50, 100, this);
         box2 = new Box(150, 100, this);
-        box3 = new Box(250, 100, this);        
+        box3 = new Box(250, 100, this);
     }
 
-    public void settings(){
-            size(400,600);
+    public void settings() {
+        size(400,600);
+        
     }
 
     public void draw(){
+       background(100, 0, 0);
        box1.display();
        box2.display();
        box3.display();
+    }
 
+    public void keyPressed(){
+        if(keyCode == RIGHT) {
+            if(box1.getValue() == box2.getValue()) {
+                box2 =  startValue * 2;
+            }
+        }
     }
 }
--- src/Box.java
+++ src/Box.java
@@ -5,7 +5,7 @@ public class Box {
     private int y;
     private int size;
     private PApplet canvas;
-    private int startValue;
+    public int startValue;
 
 
     public Box(int xPos, int yPos, PApplet b) {
@@ -16,9 +16,17 @@ public class Box {
         startValue = 10;
     }
     public void display(){
+        canvas.fill(255);
         canvas.square(x, y, size);
+        canvas.fill(0);
+        canvas.textSize(50);
+        canvas.text(startValue, x + 25, y + 75);
     }
     public void boxValue(){
-        text(startValue);
+        
     }   
+
+    public int getValue(){
+        return startValue;
+    }
 }
