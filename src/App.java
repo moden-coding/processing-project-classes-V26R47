c1b222e34da1f8bfe5321d70ae65745e121ad9f5
V26R47 <28vrichmondshazeer@gannacademy.org>
2024-12-17 09:11:37 -0500

    commit

--- src/App.java
+++ src/App.java
@@ -2,24 +2,24 @@ import processing.core.*;
 import java.util.ArrayList;
 
 public class App extends PApplet{
-    Box box1;
-    Box box2;
-    Box box3;
+    // Box box1;
+    // Box box2;
+    // Box box3;
     ArrayList<Box> boxes;
     public static void main(String[] args)  {
         PApplet.main("App");
     }
 
     public void setup(){
-        box1 = new Box(50, 100, this);
-        box2 = new Box(150, 100, this);
-        box3 = new Box(250, 100, this);
+        // box1 = new Box(50, 100, this);
+        // box2 = new Box(150, 100, this);
+        // box3 = new Box(250, 100, this);
         boxes = new ArrayList<>();
-        box1 = new Box(50, 100, this);
-        int count = 0;
-        if(count > 0){
-            boxes.add(box1 + 100, 0, this);
-            
+        int x = 50;
+        for(int i = 0; i < 3; i++){
+            Box box = new Box(x, 100, this);
+            boxes.add(box);
+            x += 100;
         }
 
     }
@@ -31,32 +31,40 @@ public class App extends PApplet{
 
     public void draw(){
        background(100, 0, 0);
-       box1.display();
-       box2.display();
-       box3.display();
+       for(Box b: boxes){
+        b.display();
+       }
+    //    box1.display();
+    //    box2.display();
+    //    box3.display();
     }
 
     public void keyPressed(){
-        if(keyCode == ' '){
-            boxes.add(box);
-        }
-        if(keyCode == RIGHT) {
-            if(box1.getValue() == box2.getValue()) {
-                box2.doubleValue();
-                box1.resetValue();
-            } else if(box2.getValue() == box3.getValue()) {
-                box3.doubleValue();
-                box2.resetValue();
-            }
-        }
+        // if(keyCode == ' '){
+        //     boxes.add(box);
+        // }
+        // if(keyCode == RIGHT) {
+        //     if(box1.getValue() == box2.getValue()) {
+        //         box2.doubleValue();
+        //         box1.resetValue();
+        //     } else if(box2.getValue() == box3.getValue()) {
+        //         box3.doubleValue();
+        //         box2.resetValue();
+        //     }
+        // }
         if(keyCode == LEFT) {
-            if(box3.getValue() == box2.getValue()) {
-                box2.doubleValue();
-                box3.resetValue();
-            } else if(box2.getValue() == box3.getValue()) {
-                box1.doubleValue();
-                box2.resetValue();
+            for (int i = 0; i < boxes.size()-1; i++){
+                Box left = boxes.get(i);
+                Box mid = boxes.get(i+1);
+                // Box right = boxes.get(i+2);
+                if(mid.getValue() == left.getValue()) {
+                    left.doubleValue();
+                    mid.resetValue();    
             }
+            }// } else if(box2.getValue() == box3.getValue()) {
+            //     box1.doubleValue();
+            //     box2.resetValue();
+            // }
         }
     }
 }
