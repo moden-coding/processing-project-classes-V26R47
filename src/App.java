631203fe09b045d3b36462700d1641ef8f3975c2
V26R47 <28vrichmondshazeer@gannacademy.org>
2024-12-20 11:35:27 -0500

    commit

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App extends PApplet {
         boxes = new ArrayList<>();
         int x = 50;
         for(int i = 0; i < 4; i++){
-            Box box = new Box(x, 100, this);
+            Box box = new Box(x, 200, this);
             boxes.add(box);
             x += 100;
         }
@@ -26,55 +26,74 @@ public class App extends PApplet {
     }
 
     public void draw(){
-       background(100, 0, 0);
-       for(Box b: boxes){
-        b.display();
-       }
+        background(100, 0, 0);
+        for(Box b: boxes){
+            b.display();
+        }
+        fill(255);
+        textSize(50);
+        int index = 0;
+        int highScore = 0;
+        if(index < boxes.size() - 2){
+            if(boxes.get(index).getValue() > highScore){
+                highScore = boxes.get(index).getValue();
+                index++;
+            }
+        }
+        text("High score:" , 50, 100);
     }
 
     public void keyPressed(){
-        if(keyCode == RIGHT) {
-            System.out.println("right");
-            for (int j = 0; j < boxes.size()-1; j++){
-                Box right = boxes.get(j +2);
-                Box mid = boxes.get(j + 1);
-                for(int d = 3; d < 0; d--){
-                if(mid.getValue() == right.getValue()) {
-                    right.doubleValue();
-                    shiftValues(j);
-                    // mid.resetValue();
-                    // boxes.get(boxes.size()-1).resetValue();    
-            }
-            }
-        }
-        }
-    
-
+        // I might be able to add a new value in first and last position.
         if(keyCode == LEFT) {
             System.out.println("left");
-            for (int i = 0; i < boxes.size()-1; i++){
+            for (int i = 0; i < boxes.size() - 1; i++){
                 Box left = boxes.get(i);
-                Box mid2 = boxes.get(i + 1);
-                for(int d2 = 0; d2 < 3; d2++){
-                if(mid2.getValue() == left.getValue()) {
+                Box mid = boxes.get(i + 1);
+                
+                if(mid.getValue() == left.getValue()) {
                     left.doubleValue();
                     shiftValues(i);
-                    // mid.resetValue();
-                    boxes.get(boxes.size()-1).resetValue();    
+                    boxes.get(boxes.size() - 1).resetValue();    
+                    break;            
                 }
-                }
-            }   // } else if(box2.getValue() == box3.getValue()) {
-            //     box1.doubleValue();
-            //     box2.resetValue();
-            // }
+            }
     
         }
+
+        if(keyCode == RIGHT) {
+            System.out.println("right");
+            for (int j = boxes.size() - 2; j >= 0; j--){
+                // if(j = /*the value 0 */){
+                //     /*than shift values to the right and double */
+                // }
+                Box left = boxes.get(j);
+                Box right = boxes.get(j + 1);
+                if(left.getValue() == right.getValue()) {
+                    right.doubleValue();
+                    shiftRight(j);
+                    boxes.get(0).resetValue();
+                    break;
+            }
+        }
+        }
+    
     }
     public void shiftValues(int index){
         for(int start = index + 1; start < boxes.size() - 1; start++){
             Box currentBox = boxes.get(start);
             Box nextBox = boxes.get(start + 1);
             currentBox.setValue(nextBox.getValue());
+            // boxes.get(boxes.size() - 1).resetValue();                
+
+        }
+        // boxes.get(boxes.size() - 1).resetValue();
+    }
+    public void shiftRight(int rightIndex){
+        for(int start = rightIndex; start > 0; start--){
+            Box right = boxes.get(start);
+            Box left = boxes.get(start - 1);
+            right.setValue(left.getValue());
         }
     }
 }
